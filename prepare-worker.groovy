b1eaae5a1098005dfca900255fcf0e73aeb65506
azizbekmol <azizmdov@gmail.com>
2021-05-17 11:05:37 +0000

    added

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,9 +1,7 @@
 node {
     stage('Install Prerequests'){
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
-            sh " ssh -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install epel-release -y' "
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install epel-release -y' "
         }
     }
 }
-
-jdjdj
