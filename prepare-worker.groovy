17f1d7b148d1eb08ae4fde7b61d92283d8bac6d0
azizbekmol <azizmdov@gmail.com>
2021-05-17 11:42:17 +0000

    added params

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,13 +1,19 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Enter IP Address', name: 'IP', trim: true)
+    ])
+])
+
 node {
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
         stage('Install Prerequests'){
-            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install epel-release -y' "
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@${ params.IP} 'yum install epel-release -y' "
         }
         stage('Install Java'){
-            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install java-1.8.0-openjdk-devel -y' "
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@${ params.IP} 'yum install java-1.8.0-openjdk-devel -y' "
         }
         stage('Install git'){
-            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install git -y' "
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@${ params.IP} 'yum install git -y' "
         }
     }
 }
