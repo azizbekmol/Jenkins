0714cc97253ad528b2e77aed13cecdc916bafe03
azizbekmol <azizmdov@gmail.com>
2021-05-17 11:29:19 +0000

    added stages

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,7 +1,13 @@
 node {
-    stage('Install Prerequests'){
-        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+        stage('Install Prerequests'){
             sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install epel-release -y' "
+        }
+        stage('Install Java'){
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install java-1.8.0-openjdk-devel -y' "
+        }
+        stage('Install git'){
+            sh " ssh -o StrictHostKeyChecking=no -i $SSH_KEY $SSH_USERNAME@167.172.147.52 'yum install git -y' "
         }
     }
 }
