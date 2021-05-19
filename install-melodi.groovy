d4fb864a7517a3f4222df7eb811fb9e3bcf4de40
azizbekmol <azizmdov@gmail.com>
2021-05-19 06:27:39 +0000

    credentional changed

--- install-melodi.groovy
+++ install-melodi.groovy
@@ -8,11 +8,9 @@ node {
     stage("Pull repo"){
         git 'https://github.com/azizbekmol/ansible-melodi.git'
     }
-
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
-        stage('Install Melodi'){
-            ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master-key', disableHostKeyChecking: true, inventory: "${ params.IP },", playbook: 'main.yml'
-        }
+    stage('Install Melodi'){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master-key', disableHostKeyChecking: true, inventory: "${ params.IP },", playbook: 'main.yml'
     }
 }
+
 
