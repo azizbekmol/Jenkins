8c7fbd2789879ed87b870c642062deb7bbd3938b
azizbekmol <azizmdov@gmail.com>
2021-05-19 05:08:47 +0000

    melodi file

--- install-melodi.groovy
+++ install-melodi.groovy
@@ -3,12 +3,11 @@ node {
         git 'https://github.com/azizbekmol/ansible-melodi.git'
     }
 
-
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
         stage('Install Melodi'){
             sh """
                 export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i \"157.230.14.40,\" all --private-key $SSH_KEY -u $SSH_USERNAME -b main.yml 
+                ansible-playbook -i \"157.230.14.40,\" --private-key $SSH_KEY -u $SSH_USERNAME -b main.yml 
             """
         }
     }
